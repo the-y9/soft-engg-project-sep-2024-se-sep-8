14eea79d4545fa06d9843db2640b90458bbd3564
AyushV-21f3 <21f3002552@ds.study.iitm.ac.in>
2024-11-07 23:46:36 +0530

    Added Regitration Page

--- static/router.js
+++ static/router.js
@@ -1,11 +1,13 @@
 import home from './components/home.js'
 import login from './components/login.js'
+import register from './components/register.js'
 import Not_found_page from './components/Not_found_page.js'
 
 const routes = [
     { path:'/' , component : home , name:'home'},
     { path:'/login', component: login , name:'login'},
-    { path: '/404_page' , component: Not_found_page , name:'404_page'}
+    { path: '/404_page' , component: Not_found_page , name:'404_page'},
+    { path:'/register', component: register, name:'register'},
 ] 
 
 export default new VueRouter({
