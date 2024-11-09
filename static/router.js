52052c14b253499fa147701f7b2f17db25d1c8ca
shukla-nitish <shuklanitish7@gmail.com>
2024-11-09 22:35:38 +0530

    deleted generate_milestone component and made changes to router.js

--- static/router.js
+++ static/router.js
@@ -4,10 +4,13 @@ import register from './components/register.js'
 import Not_found_page from './components/Not_found_page.js'
 import instructor_dashboard from './components/instructor_dashboard.js'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import create_milestone from './components/create_milestone.js'
 import milestone_tracker from './components/milestone_tracker.js'   
 =======
 import generate_milestones from './components/generate_milestones.js'
+=======
+>>>>>>> b442ac8 (removed generate milestone component)
 import student_perfomance_prediction from './components/student_perfomance_prediction.js'
 
 >>>>>>> cc87bd2 (added new components)
@@ -22,8 +25,11 @@ const routes = [
     { path:'/milestone_tracker', component: milestone_tracker, name:'milestone_tracker'},
 =======
     { path: '/student_performance', component: student_perfomance_prediction, name: 'student_performance'},
+<<<<<<< HEAD
     { path: '/generate_milestones', component: generate_milestones, name: 'generate_milestones'}
 >>>>>>> cc87bd2 (added new components)
+=======
+>>>>>>> b442ac8 (removed generate milestone component)
 ] 
 
 export default new VueRouter({
